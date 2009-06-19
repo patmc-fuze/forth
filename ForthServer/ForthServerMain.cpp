67c3f09491843dcc8e4a17022071007a4f28d0f0
patmc <>
2009-06-19 04:54:41 +0000

    Forth server main program for Windows - just sits waiting for a client to connect

---
+++ ForthServer/ForthServerMain.cpp
@@ -0,0 +1,71 @@
+// ForthServer.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <string.h>
+#include <winsock2.h>
+#include "..\ForthLib\ForthShell.h"
+#include "..\ForthLib\ForthEngine.h"
+#include "..\ForthLib\ForthInput.h"
+#include "..\ForthLib\ForthThread.h"
+#include "..\ForthLib\ForthServer.h"
+
+using namespace std;
+
+//int _tmain(int argc, _TCHAR* argv[])
+int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
+{
+    WSADATA WsaData;
+    SOCKET ServerSocket;
+    SOCKET ClientSocket;
+    struct sockaddr_in ServerInfo;
+    int iRetVal = 0;
+    char* pszSendData = "Hello world!";
+    ForthServerShell *pShell;
+    ForthInputStream *pInStream;
+
+    WSAStartup(0x202, &WsaData);
+
+    ServerSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (ServerSocket == INVALID_SOCKET)
+    {
+        printf("error: unable to create the listening socket...\n");
+    }
+    else
+    {
+        ServerInfo.sin_family = AF_INET;
+        ServerInfo.sin_addr.s_addr = INADDR_ANY;
+        ServerInfo.sin_port = htons(27015);
+        iRetVal = bind(ServerSocket, (struct sockaddr*) &ServerInfo, sizeof(struct sockaddr));
+        if (iRetVal == SOCKET_ERROR)
+        {
+            printf("error: unable to bind listening socket...\n");
+        }
+        else
+        {
+            iRetVal = listen(ServerSocket, 10);
+            if (iRetVal == SOCKET_ERROR)
+            {
+                printf("error: unable to listen on listening socket...\n");
+            }
+            else
+            {
+                pShell = new ForthServerShell;
+                while (true)
+                {
+                    printf( "Waiting for a client to connect.\n" );
+                    ClientSocket = accept(ServerSocket, NULL, NULL);
+                    printf("Incoming connection accepted!\n");
+                    pInStream = new ForthServerInputStream( ClientSocket );
+                    iRetVal = pShell->Run( pInStream );
+                    //send(ClientSocket, pszSendData, strlen(pszData), 0);
+                    closesocket(ClientSocket);
+                }
+                delete pShell;
+            }
+        }
+    }
+    closesocket(ServerSocket);
+    WSACleanup();
+    return 0;
+}
