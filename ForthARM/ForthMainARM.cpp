dd270b8c6b6bcef5a0367c3cf0a7ea7dde93e19e
Pat McElhatton <pat.mcelhatton@rcn.com>
2013-11-02 19:08:37 -0400

    fix some errors in int comparison ops add tests of int comparison ops add push/pop/negate pseudo ops to ARM assembler

--- ForthARM/ForthMainARM.cpp
+++ ForthARM/ForthMainARM.cpp
@@ -46,24 +46,36 @@ int kbhit (void)
 // from http://stackoverflow.com/questions/7469139/what-is-equivalent-to-getch-getche-in-linux
 int getch()
 {
-    char buf=0;
+    char buf = 0;
     changemode( 0 ); 
-    struct termios old={0};
+    struct termios oldSettings = {0};
+    struct termios newSettings = {0};
     fflush(stdout);
-    if(tcgetattr(0, &old)<0)
-        perror("tcsetattr()");
-    old.c_lflag&=~ICANON;
-    old.c_lflag&=~ECHO;
-    old.c_cc[VMIN]=1;
-    old.c_cc[VTIME]=0;
-    if(tcsetattr(0, TCSANOW, &old)<0)
+    
+    if ( tcgetattr(0, &oldSettings) < 0 )
+	{
+        perror("tcgetattr()");
+    }
+    if ( tcgetattr(0, &newSettings) < 0 )
+	{
+        perror("tcgetattr()");
+    }
+    newSettings.c_lflag &= ~(ICANON | ECHO);
+    newSettings.c_cc[VMIN] = 1;
+    newSettings.c_cc[VTIME] = 0;
+    if ( tcsetattr(0, TCSANOW, &newSettings) < 0 )
+    {
         perror("tcsetattr ICANON");
+    }
     if(read(0,&buf,1)<0)
+    {
         perror("read()");
-    old.c_lflag|=ICANON;
-    old.c_lflag|=ECHO;
-    if(tcsetattr(0, TCSADRAIN, &old)<0)
+    }
+    //old.c_lflag |= (ICANON | ECHO);
+    if ( tcsetattr(0, TCSADRAIN, &oldSettings) < 0 )
+	{
         perror ("tcsetattr ~ICANON");
+    }
     return (int) buf;
 }
 
