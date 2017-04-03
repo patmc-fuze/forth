d7a8f4f7eed114145ceff78a9d46afa74d97ef11
Pat McElhatton <pat.mcelhatton@rcn.com>
2017-04-03 19:50:54 -0400

    fix a warning

--- ForthOSX/ForthLoggerOSX.cpp
+++ ForthOSX/ForthLoggerOSX.cpp
@@ -12,11 +12,12 @@
 void runLogger()
 {
     int fd = -1;
-    char* myfifo = "/tmp/forthLoggerFIFO";
+    const char* myfifo = "/tmp/forthLoggerFIFO";
     char buf[MAX_BUF];
 
 	unlink(myfifo);
     /* open, read, and display the message from the FIFO */
+    printf("##################################################################################\n");
     printf("\nWaiting for named fifo connect on %s.  Hit CONTROL-C to exit.\n", myfifo);
     fd = open(myfifo, O_RDONLY);
 	while (fd < 0)
