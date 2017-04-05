8fdd6736143fc9ea7afb4303cb66bf54992dc6a7
Pat McElhatton <pat.mcelhatton@rcn.com>
2017-04-05 19:14:18 -0400

    get linux build working again after switching over to a single x86 InnerInterp.asm using nasm

---
+++ ForthLinux/ForthMain.cpp
@@ -0,0 +1,92 @@
+//
+//  main.cpp
+//  Forth
+//
+//  Created by Pat McElhatton on 3/28/17.
+//  Copyright © 2017 Pat McElhatton. All rights reserved.
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include "Forth.h"
+#include "ForthShell.h"
+
+static int loggerFD = -1;
+
+void OutputToLogger(const char* pBuffer)
+{
+    if (loggerFD < 0)
+    {
+        const char* myfifo = "/tmp/forthLoggerFIFO";
+        
+        /* create the FIFO (named pipe) */
+        unlink(myfifo);
+        if (mkfifo(myfifo, 0666) < 0)
+        {
+            perror("error making fifo");
+        }
+        loggerFD = open(myfifo, O_WRONLY);
+    }
+    write(loggerFD, pBuffer, strlen(pBuffer) + 1);
+    //close(loggerFD);
+    
+    /* remove the FIFO */
+    //unlink(myfifo);
+}
+
+int main(int argc, const char * argv[])
+{
+    int nRetCode = 0;
+    ForthShell *pShell = NULL;
+    ForthInputStream *pInStream = NULL;
+    
+    /*
+     if ( !InitSystem() )
+    {
+        nRetCode = 1;
+    }
+    else*/
+    {
+        nRetCode = 1;
+        pShell = new ForthShell;
+        pShell->SetCommandLine( argc, (const char **) (argv));
+        //pShell->SetEnvironmentVars( (const char **) envp );
+#if 0
+        if ( argc > 1 )
+        {
+            
+            //
+            // interpret the forth file named on the command line
+            //
+            FILE *pInFile = fopen( argv[1], "r" );
+            if ( pInFile != NULL )
+            {
+                pInStream = new ForthFileInputStream(pInFile);
+                nRetCode = pShell->Run( pInStream );
+                fclose( pInFile );
+                
+            }
+        }
+        else
+#endif
+        {
+            //
+            // run forth in interactive mode
+            //
+            pInStream = new ForthConsoleInputStream;
+            nRetCode = pShell->Run( pInStream );
+            
+        }
+        delete pShell;
+    }
+    
+    return nRetCode;
+}
+
+
+
