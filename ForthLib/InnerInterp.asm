c9ce2ffb8da787ad35b6208de6afcda88f6fc641
Pat McElhatton <pat.mcelhatton@rcn.com>
2018-03-31 01:03:07 -0400

    Add reverse, clz and ctz opcodes (windows only)

--- ForthLib/InnerInterp.asm
+++ ForthLib/InnerInterp.asm
@@ -4807,6 +4807,61 @@ rotate64Bop_1:
 	jmp	edi
 
 ;========================================
+entry reverseBop
+    ; Knuth's algorithm
+    ; a = (a << 15) | (a >> 17);
+	mov	eax, [edx]
+	rol	eax, 15
+    ; b = (a ^ (a >> 10)) & 0x003f801f;
+	mov	ebx, eax
+	shr	ebx, 10
+	xor	ebx, eax
+	and	ebx, 03F801Fh
+    ; a = (b + (b << 10)) ^ a;
+	mov	ecx, ebx
+	shl	ecx, 10
+	add	ecx, ebx
+	xor	eax, ecx
+    ; b = (a ^ (a >> 4)) & 0x0e038421;
+	mov	ebx, eax
+	shr	ebx, 4
+	xor	ebx, eax
+	and	ebx, 0E038421h
+    ; a = (b + (b << 4)) ^ a;
+	mov	ecx, ebx
+	shl	ecx, 4
+	add	ecx, ebx
+	xor	eax, ecx
+    ; b = (a ^ (a >> 2)) & 0x22488842;
+	mov	ebx, eax
+	shr	ebx, 2
+	xor	ebx, eax
+	and	ebx, 022488842h
+    ; a = (b + (b << 2)) ^ a;
+	mov	ecx, ebx
+	shl ecx, 2
+	add	ebx, ecx
+	xor	eax, ebx
+	mov	[edx], eax
+	jmp edi
+
+;========================================
+
+entry countLeadingZerosBop
+	mov	eax, [edx]
+	lzcnt	ebx, eax
+	mov	[edx], ebx
+	jmp edi
+
+;========================================
+
+entry countTrailingZerosBop
+	mov	eax, [edx]
+	tzcnt	ebx, eax
+	mov	[edx], ebx
+	jmp edi
+
+;========================================
 
 entry archX86Bop
 entry trueBop
